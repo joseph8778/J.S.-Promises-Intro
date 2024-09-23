7a487724eea12ad40f3fff313e13b2a5ac535f65
Joseph <joseph8778@gmail.com>
2024-09-23 16:12:18 -0500

    Added equal asynch/await function

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ const emailRef = document.querySelector(".email")
 
 
 // 1. Then
-fetch("https://jsonplaceholder.typicode.com/users/1")
-    .then(response => {
-        return response.json() //Return makes entire fetch a promise of response.json
-    })
-
-   .then(data => {
-        console.log(data)
-    })
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//     .then(response => {
+//         return response.json() //Return makes entire fetch a promise of response.json
+//     })
+//    .then(data => {
+//         console.log(data)
+//         emailRef.innerHTML = data.email
+//     })
 
 // 2. Async/Await
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
+}
+
+main();
