7638dad6b843afe2c7e46f28f88ae9b91895f33c
Joseph <joseph8778@gmail.com>
2024-09-23 17:56:46 -0500

    m

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
+
 const statusRef = (document.querySelector(".status"))
 
 function getSubscriptionStatus () {
-    return new Promise((resolve, reject) => {
-        setTimeout (() => {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
             resolve("VIP")
         }, 2000)
- 
     })
 }
 
-// getSubscriptionStatus().then(response => console.log(response))
+function getVideo (subscriptionStatus) {
+    return new Promise((resolve, reject) => {
+        if (subscriptionStatus === "VIP") {
+            resolve("Show Video")
+        }
+        else if (subscriptionStatus === "FREE") {
+            resolve("Show Trailer")
+        }
+        else {
+            reject("No Video")   
+        }
+    })
+}
 
 async function main() {
-   const status = (await getSubscriptionStatus())
-    statusRef.innerHTML = status
+    const status = await getSubscriptionStatus();
+   statusRef.innerHTML = status;
+    console.log(getVideo(status))
 }
 
 main();
