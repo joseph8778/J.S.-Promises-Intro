05b135e627853171e143b03923b0f59fa16b2377
Joseph <joseph8778@gmail.com>
2024-09-23 15:26:41 -0500

    created a fetch then function. First asynchronous function utilizing backend.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
 
 // 1. Then
-fetch("https://jsonplaceholder.typicode.com/users/1").then
+fetch("https://jsonplaceholder.typicode.com/users/1").then(httpData => {
+    httpData.json().then(specData => {
+        console.log(specData)
+    })
+})
 
 // 2. Async/Await
